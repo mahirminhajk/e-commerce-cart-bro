f71bbd2b519eb9f2163edad84ae0dd074bcd717d
mahirminhajk <kmahirminhaj@gmail.com>
2022-09-29 21:21:10 +0530

    intial project & first moves

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
+var createError = require('http-errors')
 var express = require('express')
-var adminRouter = require('./routes/admin')
-var userRouter = require('./routes/users')
-
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const fileUpload = require('express-fileupload');
 
 var app = express()
+
+var adminRouter = require('./routes/admin')
+var usersRouter = require('./routes/users')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(express.static('public'))
 
-app.use('/', userRouter)
 app.use('/admin', adminRouter)
+app.use('/', usersRouter)
+app.use(fileUpload());
 
-const port = 3000
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+});
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = app;
--- routes/admin.js
+++ routes/admin.js
@@ -1,36 +1,47 @@
 var express = require('express')
+const fileUpload = require('express-fileupload')
 var router = express.Router()
 
-router.get('/', (req, res, next) => {
-    let products = [
-        {
-            name: "IPHONE 11",
-            category: "Mobile",
-            description: "one of best iphone selling phone in 2019",
-            imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-        },
-        {
-            name: "IPHONE 12",
-            category: "Mobile",
-            description: "one of best iphone selling phone in 2020",
-            imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-        },
-        {
-            name: "IPHONE 13",
-            category: "Mobile",
-            description: "one of best iphone selling phone in 2021",
-            imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-        },
-        {
-            name: "IPHONE 14",
-            category: "Mobile",
-            description: "latest iphone with new features, released in 2022",
-            imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-        }
 
-    ]
-    res.render('admin/view-poducts', { products: products, admin: true })
+
+router.get('/', (req, res, next) => {
+  let products = [
+    {
+      name: "IPHONE 11",
+      category: "Mobile",
+      description: "one of best iphone selling phone in 2019",
+      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
+    },
+    {
+      name: "IPHONE 12",
+      category: "Mobile",
+      description: "one of best iphone selling phone in 2020",
+      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
+    },
+    {
+      name: "IPHONE 13",
+      category: "Mobile",
+      description: "one of best iphone selling phone in 2021",
+      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
+    },
+    {
+      name: "IPHONE 14",
+      category: "Mobile",
+      description: "latest iphone with new features, released in 2022",
+      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
+    }
+
+  ]
+  res.render('admin/view-poducts', { products: products, admin: true })
+  req.files
 })
 
+router.get('/add-product', (req, res) => {
+  res.render('admin/add-product', { admin: true })
+})
+router.post('/add-product', fileUpload(), (req, res) => {
+  console.log(req.body);
+  console.log(req.files.photo);
+})
 
 module.exports = router;
