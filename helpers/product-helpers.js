82eb5f4a73d4e9c37a6ea2360f67d70fc208da53
mahirminhajk <kmahirminhaj@gmail.com>
2022-10-02 16:43:41 +0530

    c:part 18

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,10 +1,17 @@
 var db = require('../config/connection')
+var collection = require('../config/collections')
 
 module.exports = {
     addProduct: (product, cb) => {
-        db.get().collection('products').insertOne(product).then((data) => {
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data) => {
             var _id = (data.insertedId).toString()
             cb(_id)
+        })
+    },
+    getAllProducts: () => {
+        return new Promise(async (resolove, reject) => {
+            let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolove(products)
         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,39 +1,14 @@
 var express = require('express')
 const fileUpload = require('express-fileupload')
-var router = express.Router()
 var productHelpers = require('../helpers/product-helpers')
+var router = express.Router()
 
 
 router.get('/', (req, res, next) => {
-  let products = [
-    {
-      name: "IPHONE 11",
-      category: "Mobile",
-      description: "one of best iphone selling phone in 2019",
-      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-    },
-    {
-      name: "IPHONE 12",
-      category: "Mobile",
-      description: "one of best iphone selling phone in 2020",
-      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-    },
-    {
-      name: "IPHONE 13",
-      category: "Mobile",
-      description: "one of best iphone selling phone in 2021",
-      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-    },
-    {
-      name: "IPHONE 14",
-      category: "Mobile",
-      description: "latest iphone with new features, released in 2022",
-      imageLink: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-7inch-gold?wid=5120&hei=2880&fmt=jpeg&qlt=90&.v=1660754254594"
-    }
+  productHelpers.getAllProducts().then((products) => {
+    res.render('admin/view-poducts', { products: products, admin: true })
+  })
 
-  ]
-  res.render('admin/view-poducts', { products: products, admin: true })
-  req.files
 })
 
 router.get('/add-product', (req, res) => {
